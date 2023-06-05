932f2780f8fe761105c7044b039ffca0de631b7c
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-06-05 16:49:59 -0700

    loginpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,56 @@
 import io from 'socket.io-client';
 import { useState } from 'react';
 import ChatRoom from './components/ChatRoom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login';
 
-// Connecting the front end to the back end
 const socket = io.connect('http://localhost:3001');
 
-export default function App () {
-// Use this to emit events whenever we need
-// Importing useEffect Hook
+export default function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
-  // creating use state to only show chat if you joined a room
   const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
-    if(username !== '' && room !== '') {
-      // If a username and room # exist we pass the room to join room
-      // This is sent to the back end to activate the event listerner join_room
-        socket.emit("join_room", room);
-        //Whenever join room is true then the chat will be visibile 
-        setShowChat(true);
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', room);
+      setShowChat(true);
     }
-  }
+  };
 
-  return(
-    <>
-    <div className='App bg-slate-900'>
-      {!showChat ? (
-    <div className='join-chat-container'>
-    <h3>Join Chat</h3>
-    <hr></hr>
-    <input type="text" placeholder='Name' onChange={(event) => {setUsername(event.target.value)}}/>
-    <hr></hr>
-    <input type="text" placeholder='Room ID..' onChange={(event) => {setRoom(event.target.value)}}/>
-    <hr></hr>
-    {/* On click the join room function is called */}
-    <button className="rounded-md 
-    bg-indigo-600 px-3 py-2 text-sm font-semibold text-white 
-    shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 
-    focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-    onClick={joinRoom}>Join a room</button>
-    </div>
-      )
-  : (
-    <ChatRoom socket={socket} username={username} room={room} />
-  ) }
-  {/* Line 39 is Passing socket into the ChatRoom component */}
-    </div>
-    </>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              showChat ? (
+                <ChatRoom socket={socket} username={username} room={room} />
+              ) : (
+                <div className='join-chat-container'>
+                  <h3>Join Chat</h3>
+                  <hr></hr>
+                  <input
+                    type="text"
+                    placeholder='Name'
+                    onChange={(event) => { setUsername(event.target.value) }}
+                  />
+                  <hr></hr>
+                  <input
+                    type="text"
+                    placeholder='Room ID..'
+                    onChange={(event) => { setRoom(event.target.value) }}
+                  />
+                  <hr></hr>
+                  <button onClick={joinRoom}>Join a room</button>
+                </div>
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
